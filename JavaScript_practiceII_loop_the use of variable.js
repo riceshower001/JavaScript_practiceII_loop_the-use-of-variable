d605386b70005dd3c900b10918731cd3508990fd
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-26 22:14:02 +0900

    Add files via upload

---
+++ JavaScript_practiceII_loop_the use of variable.js
@@ -0,0 +1,19 @@
+// Please define the variable "number".
+let number = 0 ;
+
+// Add 1 to the variable "number" and print the value of the variable "number".
+number += 1 ;
+console.log(number);
+
+// Please paste the above two lines of code four times.
+number += 1 ;
+console.log(number);
+
+number += 1 ;
+console.log(number);
+
+number += 1 ;
+console.log(number);
+
+number += 1 ;
+console.log(number);
